0835449a76c2fe62fa8a8ad35b1bb9dba1c6ec53
Sarthak Neogi <sarthakneogi@gmail.com>
2026-08-16 01:43:33 +0530

    added clicking feature, some tweeks and disabled webcam feed

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ model_pipe = "models/hand_landmarker.task"
 lastcallback = None
 pointer_loc = None
 pointer_loc_last = None
-move_sens = 0.5
+move_sens = 1.5
 move_vector = None
 
 # Setting-up Capture
@@ -35,7 +35,7 @@ options = HandLandmarkerOptions(
     base_options=BaseOptions(model_asset_path=model_pipe),
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=handle_result,
-    min_hand_detection_confidence = 0.5,
+    min_hand_detection_confidence = 0.2,
     min_hand_presence_confidence = 0.1,
     min_tracking_confidence = 0.1)
 
@@ -60,30 +60,41 @@ with HandLandmarker.create_from_options(options) as landmarker:
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
         landmarker.detect_async(mp_image, timestamp_ms)
 
-        #Moving Mouse
+        # Mouse Control
         mouse_loc = np.array(pyautogui.position())
         if lastcallback and lastcallback.hand_landmarks:
             if lastcallback.hand_landmarks[0]:
                 index_finger = np.array([lastcallback.hand_landmarks[0][8].x,lastcallback.hand_landmarks[0][8].y])
                 thumb_finger = np.array([lastcallback.hand_landmarks[0][4].x,lastcallback.hand_landmarks[0][4].y])
-                dist = np.linalg.norm(index_finger - thumb_finger)
-                if dist < 0.06:
+                middle_finger = np.array([lastcallback.hand_landmarks[0][12].x,lastcallback.hand_landmarks[0][12].y])
+                
+                move_dist = np.linalg.norm(index_finger - thumb_finger)
+                click_dist = np.linalg.norm(middle_finger - thumb_finger)
+                
+                # Moving Mouse
+                if move_dist < 0.03:
                     pointer_loc = np.array([lastcallback.hand_landmarks[0][8].x * screen_x,lastcallback.hand_landmarks[0][8].y * screen_y])
                     if pointer_loc_last is not None:
                         move_vector = (pointer_loc - pointer_loc_last) * move_sens
                         mouse_loc = mouse_loc + move_vector
-                        pyautogui.moveTo(mouse_loc[0], mouse_loc[1])
+                        pyautogui.moveTo(mouse_loc[0], mouse_loc[1], duration=0.1)
                     pointer_loc_last = pointer_loc
-                    print(mouse_loc[0], " : ",mouse_loc[1])
                 else:
                     pointer_loc = None
                     pointer_loc_last = None
+
+                # Clicking
+                if click_dist < 0.03:
+                    pyautogui.click(interval=0.1)
+                
+                print(mouse_loc[0], " : ",mouse_loc[1])
+
             else:
                 pointer_loc = None
                 pointer_loc_last = None
 
         # Displaying Webcam Feed
-        cv2.imshow('Webcam Feed', frame)
+        # cv2.imshow('Webcam Feed', frame)
 
         # Handling Exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
